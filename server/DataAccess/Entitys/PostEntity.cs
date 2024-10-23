7c33f1d287f4c79f8604dbb565ff6522c381a0e8
Razglidai <53232884+Razglidai@users.noreply.github.com>
2024-10-23 22:21:33 +0300

    сделал пару entity

--- server/DataAccess/Entitys/PostEntity.cs
+++ server/DataAccess/Entitys/PostEntity.cs
@@ -1,5 +1,10 @@
 namespace CodeCombat.DataAccess.Entity;
 public class PostEntity
 {
-    
+    public string Author {get;set;} = string.Empty;
+    public string Title {get;set;} = string.Empty; 
+    public string Desc {get;set;} = string.Empty;
+    public List<string> Tags {get;set;} = new List<string>();
+    public string Post = string.Empty;
+    public List<CommentEntity> comments = new List<CommentEntity>();
 }
